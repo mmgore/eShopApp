bd78cbb085a0c79367774ac4e3fe290704bd0696
Murat GÖRE <m.muratgore@hotmail.com>
2021-04-26 23:04:27 +0300

    added catalog item entity

---
+++ src/Sevices/Catalog/Catalog.API/Interfaces/ICatalogRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Interfaces
+{
+    public interface ICatalogRepository
+    {
+        Task<IEnumerable<CatalogItem>> GetCatalogItems();
+        Task<CatalogItem> GetCatalogItem();
+        Task<IEnumerable<CatalogItem>> GetCatalogByName(string name);
+        Task<IEnumerable<CatalogItem>> GetCatalogByCategory(string category);
+        Task CreateCatalogItem(CatalogItem item);
+        Task<bool> UpdateCatalogItem(CatalogItem item);
+        Task<bool> DeleteCatalogItem(string id);
+    }
+}
