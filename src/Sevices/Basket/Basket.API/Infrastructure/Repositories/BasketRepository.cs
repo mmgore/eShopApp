259a39717d9cbc3d55fefebc4de8af409f3a7c32
Murat GÖRE <m.muratgore@hotmail.com>
2021-06-01 00:01:46 +0300

    added repositories to basket api

---
+++ src/Sevices/Basket/Basket.API/Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Basket.API.Entities;
+using Basket.API.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Infrastructure.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task DeleteBasketAsync(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<CustomerBasket> GetBasketAsync(string username)
+        {
+            var basket = await _redisCache.GetStringAsync(username);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<CustomerBasket>(basket);
+        }
+
+        public async Task<CustomerBasket> UpdateBasket(CustomerBasket basket)
+        {
+            await _redisCache.SetStringAsync(basket.Username, JsonConvert.SerializeObject(basket));
+            return await GetBasketAsync(basket.Username);
+        }
+    }
+}
---
+++ src/Sevices/Basket/Basket.API/Interfaces/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string username);
+        Task<CustomerBasket> UpdateBasket(CustomerBasket basket);
+        Task DeleteBasketAsync(string userName);
+    }
+}
