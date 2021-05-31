6a724729760f3604656549c668fa64fc8316fd0a
Murat GÖRE <m.muratgore@hotmail.com>
2021-05-31 23:02:43 +0300

    added enitties to basket api

---
+++ src/Sevices/Basket/Basket.API/Entities/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Entities
+{
+    public class BasketItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ src/Sevices/Basket/Basket.API/Entities/CustomerBasket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class CustomerBasket
+    {
+        public string Username { get; set; }
+        public List<BasketItem> Items { get; set; }
+        public CustomerBasket(string username)
+        {
+            Username = username;
+            Items = new List<BasketItem>();
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                Items.ForEach(item =>
+                {
+                    totalPrice += item.Price * item.Quantity;
+                });
+                return totalPrice;
+            }
+        }
+    }
+}
