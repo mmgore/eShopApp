2d38e6923e4768d8f75e4de87cae62d4d904995f
Murat GÖRE <m.muratgore@gmail.com>
2021-11-19 23:03:33 +0300

    added aggregates to domain

---
+++ src/Sevices/Ordering/Ordering.Domain/AggregateModel/BuyerAggregate/Buyer.cs
@@ -0,0 +1,19 @@
+﻿using Ordering.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.AggregateModel.BuyerAggregate
+{
+    public class Buyer : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardName { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
--- src/Sevices/Ordering/Ordering.Domain/AggregateModel/OrderAggregate/Order.cs
+++ src/Sevices/Ordering/Ordering.Domain/AggregateModel/OrderAggregate/Order.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ordering.Domain.Exceptions;
+using Ordering.Domain.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,42 @@ using System.Threading.Tasks;
 
 namespace Ordering.Domain.AggregateModel.OrderAggregate
 {
-    public class Order
+    public class Order : Entity
     {
-        public int Id { get; set; }
+        public Guid BuyerId { get; private set; }
+        public string Username { get; private set; }
+        public decimal TotalPrice { get; private set; }
+        public string EmailAddress { get; private set; }
+        public string AddressLine { get; private set; }
+        public string Country { get; private set; }
+        public string State { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public Order()
+        {
+        }
+
+        public Order(Guid buyerId, string username, decimal totalPrice, string emailAddress, string country, string state, string zipCode)
+        {
+            Id = Guid.NewGuid();
+            BuyerId = buyerId;
+            Username = !string.IsNullOrWhiteSpace(username) ? username
+                                                            : throw new OrderingDomainException("Username cannot be null");
+            TotalPrice = (totalPrice > 0) ? totalPrice
+                                          : throw new OrderingDomainException("TotalPrice must be greater than 0");
+            EmailAddress = !string.IsNullOrWhiteSpace(emailAddress) ? emailAddress
+                                                            : throw new OrderingDomainException("EmailAddress cannot be null"); ;
+            Country = !string.IsNullOrWhiteSpace(country) ? country
+                                                            : throw new OrderingDomainException("Country cannot be null"); ;
+            State = !string.IsNullOrWhiteSpace(state) ? state
+                                                            : throw new OrderingDomainException("State cannot be null"); ;
+            ZipCode = !string.IsNullOrWhiteSpace(zipCode) ? zipCode
+                                                            : throw new OrderingDomainException("ZipCode cannot be null"); ;
+        }
+
+        public static Order Create(Guid buyerId, string username, decimal totalPrice, string emailAddress, string country, string state, string zipCode)
+        {
+            return new Order(buyerId, username, totalPrice, emailAddress, country, state, zipCode);
+        }
     }
 }
