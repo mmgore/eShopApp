c11a977438eb9bc49a07ba7a9a7a6ec98e805680
Murat GÖRE <m.muratgore@gmail.com>
2021-11-20 22:32:46 +0300

    added ordering context

--- src/Sevices/Ordering/Ordering.Infrastructure/OrderingContext.cs
+++ src/Sevices/Ordering/Ordering.Infrastructure/OrderingContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.AggregateModel.BuyerAggregate;
+using Ordering.Domain.AggregateModel.OrderAggregate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,11 @@ namespace Ordering.Infrastructure
 {
     public class OrderingContext : DbContext
     {
+        public OrderingContext(DbContextOptions<OrderingContext> options): base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Buyer> Buyers { get; set; }
     }
 }
