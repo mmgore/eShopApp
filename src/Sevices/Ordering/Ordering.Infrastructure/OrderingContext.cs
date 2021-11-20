5e19927667a81c49928209fb59393c7d868448e3
Murat GÖRE <m.muratgore@gmail.com>
2021-11-20 22:38:39 +0300

    added UnitOfWork

--- src/Sevices/Ordering/Ordering.Infrastructure/OrderingContext.cs
+++ src/Sevices/Ordering/Ordering.Infrastructure/OrderingContext.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Ordering.Domain.AggregateModel.BuyerAggregate;
 using Ordering.Domain.AggregateModel.OrderAggregate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure
 {
---
+++ src/Sevices/Ordering/Ordering.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using Ordering.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private bool _disposed = false;
+
+        private readonly OrderingContext _appDbContext;
+        public UnitOfWork(OrderingContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _appDbContext.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
