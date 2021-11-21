b9110b3ce29ad1906a2286f3dd10d731b2a60386
Murat GÖRE <m.muratgore@gmail.com>
2021-11-21 21:26:42 +0300

    added entity configrations

---
+++ src/Sevices/Ordering/Ordering.Infrastructure/EntityConfigrations/BuyerEntityTypeConfigration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.AggregateModel.BuyerAggregate;
+
+namespace Ordering.Infrastructure.EntityConfigrations
+{
+    internal class BuyerEntityTypeConfigration : IEntityTypeConfiguration<Buyer>
+    {
+        public void Configure(EntityTypeBuilder<Buyer> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.FirstName)
+                .HasMaxLength(25)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/Sevices/Ordering/Ordering.Infrastructure/EntityConfigrations/OrderEntityTypeConfigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.AggregateModel.BuyerAggregate;
+using Ordering.Domain.AggregateModel.OrderAggregate;
+
+namespace Ordering.Infrastructure.EntityConfigrations
+{
+    public class OrderEntityTypeConfigration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Username)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(b => b.Country)
+                .HasMaxLength(25)
+                .IsRequired();
+
+            builder.HasOne<Buyer>()
+                .WithMany()
+                .IsRequired()
+                .HasForeignKey("BuyerId");
+        }
+    }
+}
