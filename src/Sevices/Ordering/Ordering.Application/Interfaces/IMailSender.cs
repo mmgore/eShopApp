beeae4aa2a40e202bdb01a356d32cd3c42b2c9f0
Murat GÖRE <m.muratgore@gmail.com>
2021-11-22 23:47:53 +0300

    added sendmailhandler

---
+++ src/Sevices/Ordering/Ordering.Application/Interfaces/IMailSender.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Domain.AggregateModel.OrderAggregate;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Interfaces
+{
+    public interface IMailSender
+    {
+        Task SendMail(Order order);
+    }
+}
---
+++ src/Sevices/Ordering/Ordering.Application/Utilities/SendEmailHandler.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Application.Interfaces;
+using Ordering.Domain.AggregateModel.OrderAggregate;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Utilities
+{
+    public class SendEmailHandler : IMailSender
+    {
+        public async Task SendMail(Order order)
+        {
+        }
+    }
+}
