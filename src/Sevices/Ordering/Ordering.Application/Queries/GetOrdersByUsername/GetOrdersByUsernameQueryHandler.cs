d1aeff78d8a35a450bbdb4787d1356c25a2ddb9b
Murat GÖRE <m.muratgore@gmail.com>
2021-11-22 23:47:30 +0300

    modified getordersbyusenamequery

--- src/Sevices/Ordering/Ordering.Application/Queries/GetOrdersByUsername/GetOrdersByUsernameQueryHandler.cs
+++ src/Sevices/Ordering/Ordering.Application/Queries/GetOrdersByUsername/GetOrdersByUsernameQueryHandler.cs
@@ -3,8 +3,6 @@ using MediatR;
 using Ordering.Domain.AggregateModel.OrderAggregate;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
