499485e3c493a09e018d2acbcf27b0a69feb1c3f
Murat GÖRE <m.muratgore@gmail.com>
2021-11-24 00:18:16 +0300

    added application validators

---
+++ src/Sevices/Ordering/Ordering.Application/Validations/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Ordering.Application.Commads.CheckoutOrder;
+
+namespace Ordering.Application.Validations
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(r => r.EmailAddress).NotNull()
+                .WithMessage("Email Address is required!");
+
+            RuleFor(r=>r.Country).NotNull();
+
+            RuleFor(r=>r.Username).NotNull();
+
+            RuleFor(r=>r.TotalPrice).NotNull()
+                .GreaterThan(0);
+
+            RuleFor(r =>r.ZipCode).NotNull();
+        }
+    }
+}
---
+++ src/Sevices/Ordering/Ordering.Application/Validations/UpdateOrderCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Ordering.Application.Commads.UpdateOrder;
+
+namespace Ordering.Application.Validations
+{
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(r => r.EmailAddress).NotNull()
+                .WithMessage("Email Address is required!");
+
+            RuleFor(r => r.Country).NotNull();
+
+            RuleFor(r => r.Username).NotNull();
+
+            RuleFor(r => r.TotalPrice).NotNull()
+                .GreaterThan(0);
+
+            RuleFor(r => r.ZipCode).NotNull();
+        }
+    }
+}
